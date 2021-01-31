80aa1fe3b397b221b4c742bcc8dd0c0e88ef4775
kabmk <78320124+kabmk@users.noreply.github.com>
2021-02-01 07:51:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,15 +6,15 @@ import traceback
 
 bot = commands.Bot(command_prefix='^')
 token = os.environ['DISCORD_BOT_TOKEN']
-await ctx.send('1')
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-await ctx.send('2')
-credentials = ServiceAccountCredentials.from_json_keyfile_name('vital-platform-303420-29489015e433.json', scope)
-await ctx.send('3')
-gc = gspread.authorize(credentials)
+#await ctx.send('1')
+#scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+#await ctx.send('2')
+#credentials = ServiceAccountCredentials.from_json_keyfile_name('vital-platform-303420-29489015e433.json', scope)
+#await ctx.send('3')
+#gc = gspread.authorize(credentials)
 
-SPREADSHEET_KEY = '1CG2ek5j5XjbEGtWQQn2JCBaCxawD5J7DGyX33Mc4nKo'
-workbook = gc.open_by_key(SPREADSHEET_KEY)
+#SPREADSHEET_KEY = '1CG2ek5j5XjbEGtWQQn2JCBaCxawD5J7DGyX33Mc4nKo'
+#workbook = gc.open_by_key(SPREADSHEET_KEY)
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -25,26 +25,5 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def reg(ctx):
-    
     await ctx.send('1')
-    SPREADSHEET_KEY = '1CG2ek5j5XjbEGtWQQn2JCBaCxawD5J7DGyX33Mc4nKo'
-    workbook = gc.open_by_key(SPREADSHEET_KEY)
-
-    worksheet = gc.open_by_key(SPREADSHEET_KEY).worksheet("シート1")
-    await ctx.send('tierを入力してください')
-    if message.author != client.user:
-        try:
-            export_message = message.contenf
-            for num in range(1,996):
-                import_value = worksheet.cell(num , 1).value
-                if len(import_value) == 0:
-                    worksheet.update_cell(num,2, export_message)
-                    return
-        except IndexError:
-            print('IndexError:'+ message.author.name)
-            pass
-    else:
-        return
-
-
 bot.run(token)
